7d1367a20e091f690b6c0f53344ccb250ed4c0a0
unknown <jahanshir.sm92@gmail.com>
2021-11-21 06:07:39 -0800

    add getconnection to AccessDao

--- src/dao/AccessDao.java
+++ src/dao/AccessDao.java
@@ -1,4 +1,22 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class AccessDao {
+    private Connection connection;
+
+    public AccessDao() throws SQLException, ClassNotFoundException {
+        connection = getConnection();
+    }
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        if (connection == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/store_book", "root", "1234567890");
+        }
+        return connection;
+    }
+
 }
