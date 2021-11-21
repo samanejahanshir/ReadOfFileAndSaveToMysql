e1d435f2f74ee21a2a068ee6c4b06554e229c08e
unknown <jahanshir.sm92@gmail.com>
2021-11-21 06:47:25 -0800

    add method setAll in courseDao

--- src/dao/CourseDao.java
+++ src/dao/CourseDao.java
@@ -1,4 +1,31 @@
 package dao;
 
-public class CourseDao {
+import models.Course;
+import models.Student;
+
+import java.sql.*;
+import java.util.List;
+
+public class CourseDao extends AccessDao {
+
+    public CourseDao() throws SQLException, ClassNotFoundException {
+
+    }
+
+    public int setAll(List<Course> courseList) throws SQLException, ClassNotFoundException {
+        if (getConnection() != null) {
+            for (Course course : courseList) {
+                String sql = String.format("INSERT INTO `course` (`id`,`name`) VALUES (%d,'%s');",
+                        course.getId(),course.getName());
+                Statement statement = getConnection().createStatement();
+                int i = statement.executeUpdate(sql);
+                if (i != 0) {
+                    return i;
+                } else {
+                    return -1;
+                }
+            }
+        }
+        return -1;
+    }
 }
