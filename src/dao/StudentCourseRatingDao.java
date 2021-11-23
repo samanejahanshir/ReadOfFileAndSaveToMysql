c29accc38263c4339c4c7cd65afacefeea5eb754
unknown <jahanshir.sm92@gmail.com>
2021-11-23 10:28:10 -0800

    fix table ratting and delete column  timeStap from it

--- src/dao/StudentCourseRatingDao.java
+++ src/dao/StudentCourseRatingDao.java
@@ -6,15 +6,16 @@ import models.StudentCourseRating;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
+import java.util.Set;
 
 public class StudentCourseRatingDao extends AccessDao{
     public StudentCourseRatingDao() throws SQLException, ClassNotFoundException {
     }
-    public int setAll(List<StudentCourseRating> studentRatingList) throws SQLException, ClassNotFoundException {
+    public int setAll(Set<StudentCourseRating> studentRatingList) throws SQLException, ClassNotFoundException {
         if (getConnection() != null) {
             for (StudentCourseRating rating : studentRatingList) {
-                String sql = String.format("INSERT INTO `student_course_rating` (`id`,`id_student`,`id_cource`,`raiting`,`commands`,`time_stamp`) VALUES (%d,%d,%d,%f,'%s','%s');",
-                        rating.getId(), rating.getIdStudent(), rating.getIdCourse(), rating.getRatting(), rating.getCommand(), rating.getTimeStamp());
+                String sql = String.format("INSERT INTO `student_course_rating` (`id`,`id_student`,`id_cource`,`raiting`,`commands`) VALUES (%d,%d,%d,%f,'%s');",
+                        rating.getId(), rating.getIdStudent(), rating.getIdCourse(), rating.getRatting(), rating.getCommand());
                 Statement statement = getConnection().createStatement();
                 statement.executeUpdate(sql);
 
